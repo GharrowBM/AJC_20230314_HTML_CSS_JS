a5ea40adeba232e6a1f39e0af690f761682fa141
GharroxBM <gharrow.games@gmail.com>
2023-03-16 10:29:58 +0100

    Début du JS

---
+++ demo_javascript/main.js
@@ -0,0 +1,8 @@
+
+// Pour créer une variable en JAvascript, il faut utiliser soit le mot-clé 'var', soit 'let'. Si l'on veut réaliser une constante, on utilise le mot-clé 'const'
+let monTexte = "Blabla"
+const monTexteB = "Titi"
+// monTexteB = 25 impossible car c'est une CONSTANTE
+
+console.log(monTexte)
+
